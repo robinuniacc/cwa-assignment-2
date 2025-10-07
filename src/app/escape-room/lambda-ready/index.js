c6be500bbaf28c685c33e0f053330f1ce1465085
RaksaRobin <21905099@students.latrobe.edu.au>
2025-10-07 22:03:55 +1100

    host escape room html template on S3
    - The assignment requirement says that must be done.
    - The html template now uses CDN URLs for dependencies

--- src/app/escape-room/lambda-ready/index.js
+++ src/app/escape-room/lambda-ready/index.js
@@ -1,12 +1,17 @@
-/* eslint-disable */
-const fs = require("fs").promises;
+const s3 = require("@aws-sdk/client-s3");
 
-const ESCAPE_ROOM_HTML_TEMPLATE_PATH = "./escape-room-template.html";
+const S3_ESCAPE_ROOM_HTML_TEMPLATE_KEY = "res/escape-room-template.html";
 const QUESTIONS_REGEX = /{{questions}}/g;
 const IMAGE_MIME_TYPE_REGEX = /{{bgImgMimeType}}/g;
 const IMAGE_SRC_REGEX = /{{bgImgBase64}}/g;
 const IMAGE_WIDTH_REGEX = /{{bgImgWidth}}/g;
 const IMAGE_HEIGHT_REGEX = /{{bgImgHeight}}/g;
+
+const S3_CLIENT = new s3.S3Client({});
+const GET_ESCAPE_ROOM_HTML_TEMPLATE_COMMAND = new s3.GetObjectCommand({
+  Bucket: process.env.BUCKET_NAME,
+  Key: S3_ESCAPE_ROOM_HTML_TEMPLATE_KEY,
+});
 
 function parseBody(event) {
   const { questions, bgImgBase64, bgImgSize, bgImgMimeType } = event;
@@ -46,10 +51,9 @@ exports.handler = async (event, context) => {
   }
 
   const { questions, bgImgBase64, bgImgSize, bgImgMimeType } = parsedBody;
-  const htmlTemplate = await fs.readFile(
-    ESCAPE_ROOM_HTML_TEMPLATE_PATH,
-    "utf-8",
-  );
+  const htmlTemplate = await S3_CLIENT.send(
+    GET_ESCAPE_ROOM_HTML_TEMPLATE_COMMAND,
+  ).then((data) => data.Body.transformToString());
   const html = htmlTemplate
     .replace(QUESTIONS_REGEX, JSON.stringify(questions, null, 2))
     .replace(IMAGE_SRC_REGEX, bgImgBase64)
